84720a47c06ca7109bc1f242af891af7cfa912db
sporeball <sporeballdev@gmail.com>
2021-07-19 18:33:59 -0500

    basic spec

--- tentamen.js
+++ tentamen.js
@@ -5,7 +5,38 @@
   MIT license
 */
 
-export default class Tentamen {
-  constructor() {
+const logUpdate = require('log-update');
+
+class Tentamen {
+  constructor(obj) {
+    this.fn = obj.fn;
+    this.before = obj.before || function(input) {
+      return input;
+    }
+    this.after = obj.after || function(input) {
+      return input;
+    }
+  }
+
+  add(title, input, expected) {
+    logUpdate(`    ${title}`);
+
+    input = (this.before)(input);
+    let output = (this.fn)(input);
+    output = (this.after)(output);
+    if (output === expected) {
+      logUpdate(`  O ${title}`);
+    } else {
+      logUpdate(`  X ${title}`);
+    }
+
+    logUpdate.done();
+  }
+
+  suite(title, fn = this.fn) {
+    this.fn = fn;
+    console.log(title);
   }
 }
+
+module.exports = Tentamen;
